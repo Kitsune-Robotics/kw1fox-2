aff71a61f725be7607b86d0000a403299b348724
KenwoodFox <kenwood364@gmail.com>
2022-07-29 23:22:52 -0400

    This does break the firmware but a fix is on the way

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -9,36 +9,17 @@
 
 #include <Arduino.h>
 
-#include <Nokia_LCD.h>
-
-// GND, Lamp, VCC, CLK, DIN, DC, CE, RST
-
-Nokia_LCD lcd(7 /* CLK */, 6 /* DIN */, 5 /* DC */, 4 /* CE */, 3 /* RST */);
-
-// Based on https://github.com/platisd/nokia-5110-lcd-library/tree/master/examples/Nokia_LCD_Demo
+#include <FreeRTOS.h>
 
 void setup()
 {
-    // Initialize the screen
-    lcd.begin();
-    // Set the contrast
-
-    lcd.setContrast(60); // Good values are usualy between 40 and 60
-
-    // Clear the screen by filling it with black pixels
-    lcd.clear(true);
-
-    delay(2000);
+    // Serial.begin();
+    Serial.println("Hello world.");
 }
 
 void loop()
 {
-    // Set the cursor on the beginning of the 6th row (the last one)
-    lcd.setCursor(0, 5);
+    Serial.println("Greatness awaits..");
 
-    // Write something on the specific row with inverted color
-    lcd.setInverted(true);
-    lcd.print("Hello world!");
-
-    delay(50);
+    delay(100);
 }
