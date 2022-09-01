5d9a5a6f0c1050e2f16cddddf7f9a8bb62232f10
KenwoodFox <kenwood364@gmail.com>
2022-09-01 00:06:31 -0400

    Switch back to picotool, and setup SDFS

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -8,13 +8,47 @@
  */
 
 #include <Arduino.h>
+#include <FreeRTOS.h>
 
-// #include <FreeRTOS.h>
+// Filesystems and SD
+#include <SDFS.h>
 
 void setup()
 {
     Serial.begin();
-    Serial.println("Hello world.");
+    delay(2000);
+
+    Serial.println("Initializing Devices.");
+    delay(500);
+
+    Serial.println("SDFS Bringup");
+    // SDFS Settings
+    SDFSConfig c2;
+    c2.setCSPin(4);
+    SDFS.setConfig(c2);
+
+    // Begin SDFS
+    if (!SDFS.begin())
+    {
+        Serial.println("Unable to bringup, formatting SPIFFS, see manual https://arduino-pico.readthedocs.io/en/latest/fs.html#begin");
+    }
+
+    Serial.println("initialization done.");
+
+    // Do some filesystem testing
+    if (!SDFS.exists("/Logs/"))
+    { // If there is no logfolder
+        SDFS.mkdir("/Logs/");
+    }
+
+    if (!SDFS.exists("/Logs/myLogFile.log"))
+    {
+        SDFS.open("/Logs/myLogFile.log", "a");
+    }
+    else
+    {
+        SDFS.open("/Logs/myLogFile.log", "a");
+    }
 
     pinMode(16, OUTPUT);
 }
@@ -23,6 +57,8 @@ void loop()
 {
     Serial.println("Greatness awaits..");
 
-    delay(100);
+    delay(1000);
     digitalWrite(16, HIGH);
+    delay(1000);
+    digitalWrite(16, LOW);
 }
